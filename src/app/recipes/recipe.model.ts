5eecd8ecfc3f08dbfb46653728d6d71f0da139ea
minarazi <razimina76@gmail.com>
2017-12-04 22:36:46 +0330

    add recipe item static

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,8 +1,8 @@
 export class Recipe {
- private name = string;
- private description = string;
- private imagePath = string;
- constructor(name = string ,desc = string ,imagePath = string )  {
+ private name : string;
+ private description : string;
+ private imagePath : string;
+ constructor(name : string ,desc : string ,imagePath : string )  {
    this.name = name;
    this.description = desc;
    this.imagePath = imagePath;
